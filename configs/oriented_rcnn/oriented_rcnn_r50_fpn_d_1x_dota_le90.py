36503c1e5ab11c3e294e6e9d5125d7b3c5351beb
doberman <2531363057@qq.com>
2024-12-19 14:41:16 +0800

    chore

--- configs/oriented_rcnn/oriented_rcnn_r50_fpn_d_1x_dota_le90.py
+++ configs/oriented_rcnn/oriented_rcnn_r50_fpn_d_1x_dota_le90.py
@@ -78,7 +78,7 @@ model = dict(
                 min_pos_iou=0.3,
                 match_low_quality=True,
                 ignore_iof_thr=-1,
-                ratio_correct=[1/2,1/2]
+                correct_list=[1/2,1/2]
                 ),
             sampler=dict(
                 type='RandomSampler',
